349bbd577aa54d874c34cf5a4798d8ab8c562b2b
Peter Krauß <peter.krauss@kit.edu>
2021-05-06 11:04:24 +0200

    made chemotion version more readable. now prefers to use the tag instead of the hash

--- build-chemotion/rebuild.sh
+++ build-chemotion/rebuild.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-VERSION=$(date +%+2y.%+2m)-3
+VERSION=$(date +%+2y.%+2m)-1
 PID=$(pwd | md5sum | cut -c -16)
 GITHASH=$(cd src; git log --pretty=format:'%h' -n 1)
-GITTAG=$(cd src; git describe --exact-match --tags ${GITHASH})
+GITTAG=$(cd src; git describe --exact-match --tags ${GITHASH} || echo ${GITHASH})
 
-echo "Building [$VERSION] based on [$GITHASH ($GITTAG)]. Press enter."
+echo "Building [$VERSION] based on [$GITTAG]. Press enter."
 read
 
 docker rm -f db
@@ -14,7 +14,7 @@ docker network rm ${PID}_net
 docker network create ${PID}_net
 docker run -d --name db --network "${PID}_net" -p 5432:5432 -e POSTGRES_HOST_AUTH_METHOD=trust -e POSTGRES_USER=root postgres
 sleep 10
-time docker build --build-arg CHEMOTION_VERSION=${VERSION}@$(git log -1 --pretty='%h') --network "${PID}_net" -t ptrxyz/chemotion:${VERSION} $@ .
+time docker build --build-arg CHEMOTION_VERSION=${VERSION}@{GITTAG} --network "${PID}_net" -t ptrxyz/chemotion:${VERSION} $@ .
 docker rm -f db
 docker network rm ${PID}_net
 
