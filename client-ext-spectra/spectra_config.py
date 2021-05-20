cff68d5c2e92beab7fcd2c483014f1b98ee5da67
Peter Krauß <peter.krauss@kit.edu>
2021-05-20 10:43:44 +0200

    Added client stuff for chemspectra

---
+++ client-ext-spectra/spectra_config.py
@@ -0,0 +1,8 @@
+SECRET_KEY = b'M\x8b\xfc\xcf\xfb\xddbK\xf1\xa8\xe2B\xe9\x9e\xf3\x17'
+
+# IP_WHITE_LIST is ignored in the current release
+IP_WHITE_LIST = ''
+
+# Not sure what this does ....
+URL_DEEPIR = 'http://127.0.0.1:3008/infer_ir'
+URL_NSHIFTDB = 'https://nmrshiftdb.nmr.uni-koeln.de/NmrshiftdbServlet/nmrshiftdbaction/quickcheck'
