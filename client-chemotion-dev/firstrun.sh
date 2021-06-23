71a647b61684dd96f1b8be7aaa127dab0e86a09f
Frank Broda <fbroda@ipb-halle.de>
2021-06-23 11:20:58 +0200

    Update firstrun.sh
    Missing directory prevents successful installation / setup

--- client-chemotion-dev/firstrun.sh
+++ client-chemotion-dev/firstrun.sh
@@ -3,6 +3,8 @@
 git clone https://github.com/ComPlat/chemotion_ELN.git src
 cd src && git checkout development ; cd ..
 
+mkdir src/node_modules
+
 echo "npm install ." | docker-compose run eln user-shell
 docker-compose run eln init-dev
 
