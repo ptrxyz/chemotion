0caf388cc3142bea48f186e93f40068619fc8481
Memood <in1767@kit.edu>
2021-07-12 11:57:48 +0200

    cleaned - some files and folders

--- build-chemotion/reconfig.sh
+++ build-chemotion/reconfig.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-${APP_DIR}/app/mustash/mo ${APP_DIR}/app/mustash/templates/env_template.mo 	        >>   ${APP_DIR}/app/.env
-${APP_DIR}/app/mustash/mo ${APP_DIR}/app/mustash/templates/database_template.mo		>>   ${APP_DIR}/app/config/database.yml
-${APP_DIR}/app/mustash/mo ${APP_DIR}/app/mustash/templates/datacollectors_template.mo	>>   ${APP_DIR}/app/config/datacollectors.yml
-#  ${APP_DIR}/app/mustash/mo ${APP_DIR}/app/mustash/templates/editors_template.mo 	>>   ${APP_DIR}/app/config/editors.yml
-#  ${APP_DIR}/app/mustash/mo ${APP_DIR}/app/mustash/templates/inference_template.mo 	>>   ${APP_DIR}/app/config/inference.yml
-${APP_DIR}/app/mustash/mo ${APP_DIR}/app/mustash/templates/secrets_template.mo 		>>   ${APP_DIR}/app/config/secrets.yml
-#  ${APP_DIR}/app/mustash/mo ${APP_DIR}/app/mustash/templates/spectra_template.mo 	>>   ${APP_DIR}/app/config/spectra.yml
-${APP_DIR}/app/mustash/mo ${APP_DIR}/app/mustash/templates/storage_template.mo 		>>   ${APP_DIR}/app/config/storage.yml
-${APP_DIR}/app/mustash/mo ${APP_DIR}/app/mustash/templates/user_props_template.mo 	>>   ${APP_DIR}/app/config/user_props.yml
+export mustash=${APP_DIR}/app/mustash/mo
+export templates=${APP_DIR}/app/mustash/templates
 
+${mustash} ${templates}/env_template.mo 		> ${APP_DIR}/app/.env
+${mustash} ${templates}/database_template.mo		> ${APP_DIR}/app/config/database.yml    
+${mustash} ${templates}/datacollectors_template.mo	> ${APP_DIR}/app/config/datacollectors.yml
+#${mustash} ${templates}/editors_template.mo 		> ${APP_DIR}/app/config/editors.yml
+#${mustash} ${templates}/inference_template.mo 		> ${APP_DIR}/app/config/inference.yml
+${mustash} ${templates}/secrets_template.mo 		> ${APP_DIR}/app/config/secrets.yml
+#${mustash} ${templates}/spectra_template.mo 		> ${APP_DIR}/app/config/spectra.yml
+${mustash} ${templates}/storage_template.mo 		> ${APP_DIR}/app/config/storage.yml
+${mustash} ${templates}/user_props_template.mo 		> ${APP_DIR}/app/config/user_props.yml
 
