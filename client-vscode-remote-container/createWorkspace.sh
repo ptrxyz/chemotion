99edf2e74176568a3c2e3fe94de2c588e91044c2
Peter Krauß <peter.krauss@kit.edu>
2021-08-04 16:23:55 +0200

    createWorkspace script does now install the extension for you and open the workspace.

--- client-vscode-remote-container/createWorkspace.sh
+++ client-vscode-remote-container/createWorkspace.sh
@@ -23,7 +23,6 @@ done
 procs=$(nproc 2>/dev/null || echo "4")
 echo "Info: ${procs} cores detected."
 
-
 echo "Creating workspace in ${dest}..."
 
 # clone the repo
@@ -39,12 +38,34 @@ cp docker-compose.vscode ${dest}/
 # adjust number of CPUs used
 sed -i 's/BUNDLE_JOBS=.*/BUNDLE_JOBS='${procs}'/g' ${dest}/docker-compose.vscode
 
-echo "done."
-echo "Please open the folder [${dest}] in VSCode and confirm"
-echo "to change to the container environment when prompted."
-echo ""
-echo "Please Note: this requires the non-OSS build of VSCode "
-echo "             with the Remote Development Extension Pack"
-echo "             installed and working."
-echo ""
+# adjust config files for chemotion
+cp ${dest}/public/welcome-message-sample.md ${dest}/welcome-message.md
+cp ${dest}/config/datacollectors.yml.example ${dest}/config/datacollectors.yml
+cp ${dest}/config/storage.yml.example ${dest}/config/storage.yml
+cp ${dest}/config/database.yml.example ${dest}/config/database.yml
+sed -i 's/host: .*/host: db/g' ${dest}/config/database.yml
 
+# Install VSCode extension
+command -v code &>/dev/null && {
+	code --install-extension ms-vscode-remote.vscode-remote-extensionpack
+} || {
+	echo "VSCode not detected. Please make sure the Remote Development Extension Pack is installed."
+	echo "Get it here: https://marketplace.visualstudio.com/items?itemName=ms-vscode-remote.vscode-remote-extensionpack"
+}
+
+echo "done."
+echo ""
+command -v code &>/dev/null && {
+	echo "Opening folder [${dest}] in VSCode for you. Please confirm"
+	echo "to change to the container environment when prompted."
+	echo ""
+	code ${dest}
+} || {
+	echo "Please open the folder [${dest}] in VSCode and confirm"
+	echo "to change to the container environment when prompted."
+	echo ""
+	echo "Please Note: this requires the non-OSS build of VSCode "
+	echo "             with the Remote Development Extension Pack"
+	echo "             installed and working."
+	echo ""
+}
