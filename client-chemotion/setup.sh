4f922b53913eb35cde3cf4bc5d065f11978d6247
Peter Krauß <peter.krauss@kit.edu>
2021-11-23 08:39:17 +0100

    Release 1.0.3D0.1; setup.sh updated

--- client-chemotion/setup.sh
+++ client-chemotion/setup.sh
@@ -6,7 +6,14 @@ mkdir -p shared/eln/uploads
 mkdir -p shared/eln/public/packs
 mkdir -p shared/eln/public/assets
 mkdir -p shared/eln/public/images/ketcherails
+mkdir -p shared/eln/public/docx
+mkdir -p shared/eln/public/zip
+mkdir -p shared/eln/public/simulations
+mkdir -p shared/eln/app/packs/src/components/extra
 mkdir -p shared/eln/public/images/molecules
 mkdir -p shared/eln/public/images/samples
+mkdir -p shared/eln/public/images/qr
+mkdir -p shared/eln/public/images/reactions
+mkdir -p shared/eln/public/images/research_plans
 mkdir -p shared/eln/config
 mkdir -p db-data
