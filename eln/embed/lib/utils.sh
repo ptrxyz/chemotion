9b7c59bbb40d940838bbf750b2ecbec77e4154c5
Peter Krauß <peter.krauss@kit.edu>
2022-05-13 18:08:19 +0200

    Release branch created.

--- eln/embed/lib/utils.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -a
-
-log() {
-    TAG="\x1B[35m[${TASKNAME}]\x1B[0m"
-    echo -en "$TAG "
-    echo $@
-}
--- eln/embed/scripts/resetAdminPW.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cd /chemotion/app && echo "u = User.find_by(type: 'Admin'); u.password='chemotion'; u.account_active=true; u.save" | rails c
