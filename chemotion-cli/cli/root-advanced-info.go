0d5a1d70e8c25cdfd8bfef5c8ca600bd917d6731
Shashank Shekhar Harivyasi <harivyasi@users.noreply.github.com>
2022-06-15 14:29:45 +0200

    separated os dependent calls

--- chemotion-cli/cli/root-advanced-info.go
+++ chemotion-cli/cli/root-advanced-info.go
@@ -2,40 +2,19 @@ package cli
 
 import (
 	"fmt"
-	"math"
 	"runtime"
 	"strings"
-	"syscall"
 
 	"github.com/spf13/cobra"
 )
 
-// helper function that is also used by infoInstanceRootCmd
+// helper function that is used by infoAdvancedRootCmd
 func systemInfo() (info string) {
 	// CPU
 	info += fmt.Sprintln("- CPU Cores:", runtime.NumCPU())
-	// Disk space
-	if runtime.GOOS == "linux" || runtime.GOOS == "darwin" {
-		var disk syscall.Statfs_t
-		if err := syscall.Statfs(workDir.String(), &disk); err == nil {
-			info += fmt.Sprintf("- Disk space:\n  - %7.1fGi (total) %7.1fGi (free)\n", float64(disk.Blocks*uint64(disk.Bsize))/math.Pow(2, 30), float64(disk.Bavail*uint64(disk.Bsize))/math.Pow(2, 30))
-		} else {
-			zboth.Warn().Err(err).Msgf("Failed to retrieve information about disk space.")
-		}
-	} else {
-		zboth.Warn().Err(fmt.Errorf("running on %s", runtime.GOOS)).Msgf("Cannot retrieve disk space information for this operating system.")
-		// TODO-maybe-v2 write implementation for windows
-	}
-	// Memory
 	if runtime.GOOS == "linux" {
-		var mem syscall.Sysinfo_t
-		if err := syscall.Sysinfo(&mem); err == nil {
-			info += fmt.Sprintf("- Memory:\n  - %7.1fGi (total) %7.1fGi (free)\n", float64(mem.Totalram)/math.Pow(2, 30), float64(mem.Freeram)/math.Pow(2, 30))
-		} else {
-			zboth.Warn().Err(err).Msgf("Failed to retrieve information about memory.")
-		}
-	} else {
-		zboth.Warn().Err(fmt.Errorf("running on %s", runtime.GOOS)).Msgf("Cannot retrieve memory information for this operating system.")
+		info += getDiskSpace() // Disk Space
+		info += getMemory()    // Memory
 	}
 	info += fmt.Sprintln("Used software versions:")
 	printVersionOf := []string{"docker", "ruby", "passenger", "node", "npm"}
