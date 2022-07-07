36f26e15e9a48af654184ab047b72133b2568646
Shashank Shekhar Harivyasi <harivyasi@users.noreply.github.com>
2022-07-07 08:10:59 +0200

    version bump

--- chemotion-cli/cli/root.go
+++ chemotion-cli/cli/root.go
@@ -41,7 +41,7 @@ import (
 )
 
 const (
-	versionCLI            = "0.1.1-alpha"
+	versionCLI            = "0.1.2-alpha"
 	versionYAML           = "1.0"
 	nameCLI               = "Chemotion"
 	defaultConfigFilepath = "chemotion-cli.yml"
