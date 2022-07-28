b2f87edea3bc44bed9772db3ab5f71d768b7375a
harivyasi <45761248+harivyasi@users.noreply.github.com>
2022-07-28 15:35:51 +0200

    version 0.1.6-alpha
    now include compose file in release

--- chemotion-cli/cli/root.go
+++ chemotion-cli/cli/root.go
@@ -41,7 +41,7 @@ import (
 )
 
 const (
-	versionCLI             = "0.1.5-alpha"
+	versionCLI             = "0.1.6-alpha"
 	versionYAML            = "1.0"
 	nameCLI                = "Chemotion"
 	defaultConfigFilepath  = "chemotion-cli.yml"
