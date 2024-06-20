697a91ce777e7343371441eb1d925d296b1ee951
Shashank S. Harivyasi <45761248+harivyasi@users.noreply.github.com>
2024-06-20 11:56:39 +0200

    Fix #24 ask bundle to run rails (#30)

--- eln/additives/embed/scripts/resetAdminPW.sh
+++ eln/additives/embed/scripts/resetAdminPW.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 cd /chemotion/app || exit 200
-echo "u = User.find_by(type: 'Admin'); u.password='chemotion'; u.account_active=true; u.save" | rails c
+echo "u = User.find_by(type: 'Admin'); u.password='chemotion'; u.account_active=true; u.save" | bundle exec rails c
